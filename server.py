e34f76a3a539701004f20201ebd6e8bbb4d7fdf7
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2011-12-01 00:38:24 +0100

    refactoring, comments added, only one try to connect to jabber server

--- server.py
+++ server.py
@@ -5,24 +5,25 @@ import os
 import re
 import cgi
 import mimetypes
-#from string import Template
+from string import Template
 
 from jabber import RosterUtility
 import tools
 
 
+version = '0.1'
+
 
 def index(environ, start_response):
-    #template = Template(open('templates/index.html').read())
-    #return [template.substitute(dict(content='Hello World Application'))]
+    """Main view."""
     templates_path = environ.get('TEMPLATES_PATH', 'templates')
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [open(templates_path + '/index.html').read()]
+    body = Template(open(templates_path + '/index.html').read()).substitute(dict(version=version))
+    start_response('200 OK', [('Content-Type', 'text/html'), ('Content-length', str(len(body)))])
+    return [body]
 
 
 def roster(environ, start_response):
-    #parameters = cgi.parse_qs(environ.get('QUERY_STRING', ''))
-    #print parameters
+    """Roster retrieve or update."""
     form = cgi.FieldStorage(fp=environ['wsgi.input'],
                             environ=environ,
                             keep_blank_values=True)
@@ -48,33 +49,41 @@ def roster(environ, start_response):
     if not host:
         host = tools.get_host_from_email(jid)
 
-    ret_str = ''
+    body = ''
+    error = False
     if retrieve is not None:
+        # retrieve roster
         xmpp = RosterUtility(jid, password)
-        if xmpp.connect((host, port)):
+        if xmpp.connect((host, port), reattempt=False):
             xmpp.process(threaded=False)
-            ret_str = xmpp.get_client_roster_as_csv()
+            body = xmpp.get_client_roster_as_csv()
         else:
-            start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
-            return "Unable to connect to XMPP server."
+            error = True
 
     elif update is not None:
+        # update roster
         roster = form.getvalue('roster')
         xmpp = RosterUtility(jid, password, roster)
-        if xmpp.connect((host, port)):
+        if xmpp.connect((host, port), reattempt=False):
             xmpp.process(threaded=False)
-            ret_str = roster
+            body = roster
         else:
-            start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
-            return "Unable to connect to XMPP server."
-
-    start_response('200 OK', [('Content-Type', 'text/plain'), ('Content-length', str(len(ret_str)))])
-    return [str(ret_str)]
+            error = True
+            
+    if error:
+        body = 'Unable to connect to XMPP server.'
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain'), ('Content-length', str(len(body)))])
+    else:
+        start_response('200 OK', [('Content-Type', 'text/plain'), ('Content-length', str(len(body)))])
+        
+    return [str(body)]
 
 
 def static(environ, start_response):
-
+    """Serve static files"""
+    
     def send_file(file_path, size):
+        """Generator that yields chunks of static files so they don't have to be loaded into memory."""
         BLOCK_SIZE = 1024
         with open(file_path) as f:
             block = f.read(BLOCK_SIZE)
